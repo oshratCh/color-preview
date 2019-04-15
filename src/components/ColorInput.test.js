b1c0753efffe094d743fe560c84519c7da090f10
Oshrat Charazi <oshrat@date4dos.co.il>
2019-04-15 09:23:15 +0300

    fix test file

--- src/components/ColorInput.test.js
+++ src/components/ColorInput.test.js
@@ -1,13 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { render, fireEvent, getByTestId } from "react-testing-library";
+import {render, fireEvent, cleanup, waitForElement} from 'react-testing-library'
 
 import ColorInput from "./ColorInput";
 
-// Oshrat - NOTE: This test failed, I'm working on it.
-it("renders without crashing", () => {
-  const { container, rerender } = render(<ColorInput />);
-  const countValue = getByTestId(container, "colorInput");
-  fireEvent.change(countValue, { target: { value: "1" } });
-  expect(countValue.textContent).toBe("#1");
+afterEach(cleanup)
+
+const setup = () => {
+  const utils = render(<ColorInput colorChange={()=>{}}/>)
+  const input = utils.getByTestId('colorInput')
+  return {
+    input,
+    ...utils,
+  }
+}
+
+test("renders default input", () => {
+  const {input} = setup()
+  expect(input.value).toBe('#')
+});
+
+test("handle valid input", () => {
+  const {input} = setup()
+  fireEvent.change(input, {target: {value: '#23'}})
+  expect(input.value).toBe('#23')
+});
+
+
+test("handle invalid input", () => {
+  const {input} = setup()
+  fireEvent.change(input, {target: {value: '#$'}})
+  expect(input.value).toBe('#')
+});
+
+test("handle valid then invalid input", () => {
+  const {input} = setup()
+  fireEvent.change(input, {target: {value: '#2'}})
+  fireEvent.change(input, {target: {value: '#2$'}})
+  expect(input.value).toBe('#2')
 });
